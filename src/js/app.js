0fee65b31ea3e65ddac7ef89a07f272bf2aad0b8
Milana <durckina.milana2016@yandex.ru>
2023-05-10 19:00:30 +0300

    error

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
 export default function sorting(players) {
-    const key = 'health';
-    return players.sort((player1, player2) => player2[key] - player1[key]);
-  }
+  const key = 'health';
+  return players.sort((player1, player2) => player2[key] - player1[key]); }
